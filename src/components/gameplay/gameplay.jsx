6170e6827a3275334921531c88ce9fbfff44fbda
Kep <fynjycloud@gmail.com>
2021-07-08 17:56:39 +0300

    add config.json

--- src/components/gameplay/gameplay.jsx
+++ src/components/gameplay/gameplay.jsx
@@ -9,12 +9,15 @@ import umbrellaFive from '../../assetes/zont5.png'
 import continueButton from '../../assetes/knopka.png'
 import useSound from "use-sound";
 import click from "../../sounds/click.mp3";
+import config from '../../config.json'
 
 const Gameplay = () => {
 
     const [score, setScore] = useState(0);
-    const [foundUmbrellaSound] = useSound(click)
 
+    const [foundUmbrellaSound] = useSound(click);
+
+    const allUmbrellas = config.objects;
 
     const addScore = (element) => {
         setScore(score + 1)
@@ -27,20 +30,21 @@ const Gameplay = () => {
             return 'viewButton'
         }
     }
+
+    const mapAllUmbrellas = () => {
+        return allUmbrellas.map(u => <img onClick={(e) => addScore(e.target)}
+                                   className='gameplay__umbrella'
+                                   src={u.url}
+                                   alt='umbrella-icon'
+                                   style={{top: u.top + 'px', left: u.left + 'px', width: u.width + 'px', height: u.height + 'px'}}/>)
+    }
     return (
         <div className='gameplay'>
             <img className='gameplay__background' src={gameplayBackground} alt="gameplay"/>
             <div className="gameplay__counter">{score}/5</div>
-            <img onClick={(e) => addScore(e.target)} className='gameplay__umbrella-one'
-                 src={umbrellaOne} alt="umbrella-one"/>
-            <img onClick={(e) => addScore(e.target)} className='gameplay__umbrella-two'
-                 src={umbrellaTwo} alt="umbrella-two"/>
-            <img onClick={(e) => addScore(e.target)} className='gameplay__umbrella-three'
-                 src={umbrellaThree} alt="umbrella-three"/>
-            <img onClick={(e) => addScore(e.target)} className='gameplay__umbrella-four'
-                 src={umbrellaFour} alt="umbrella-four"/>
-            <img onClick={(e) => addScore(e.target)} className='gameplay__umbrella-five'
-                 src={umbrellaFive} alt="umbrella-five"/>
+            {
+                mapAllUmbrellas()
+            }
             <img className={'gameplay__button ' + viewButton()} src={continueButton} alt='continue-button'/>
         </div>
     )
